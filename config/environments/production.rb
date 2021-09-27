b4c26f7b466bc4fd5726a040ea7249b45fa8fe92
Scott Jodoin <scottjodoin@gmail.com>
2021-09-26 21:31:25 -0500

    add heroku production information

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,5 +112,11 @@ Rails.application.configure do
   
   # DEVISE: In production, :host should be set to the actual host of your application.
   # * Required for all applications. *
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
+  end
 end
